254e39787fbed7300bfb5723bb2af8e983ffbe8b
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-02-01 16:51:15 +0200

    Add files via upload

---
+++ src/main/java/com/coderscampus/AssignmentSubmission/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.coderscampus.AssignmentSubmission.service;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.AssignmentSubmission.domain.User;
+import com.coderscampus.AssignmentSubmission.repository.UserRepository;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	//@Autowired
+	//private CustomPasswordEncoder passwordEncoder;
+	@Autowired
+	private UserRepository userRepo;
+	 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User>userOpt=userRepo.findByUsername(username);
+		
+		return userOpt.orElseThrow(()->new UsernameNotFoundException("Invaild credntials"));
+	}
+
+}
