c242e2858808b25c66e762c433d6bef95dc861a8
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-02-10 04:48:26 +0200

    Update AuthController.java

--- src/main/java/com/coderscampus/AssignmentSubmission/web/AuthController.java
+++ src/main/java/com/coderscampus/AssignmentSubmission/web/AuthController.java
@@ -1,6 +1,13 @@
 package com.coderscampus.AssignmentSubmission.web;
 
+import java.lang.System.Logger.Level;
+import java.util.logging.Logger;
+
+import org.apache.catalina.startup.UserDatabase;
+
+
 import org.apache.commons.logging.Log;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 
@@ -10,16 +17,29 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coderscampus.AssignmentSubmission.domain.Assignment;
 import com.coderscampus.AssignmentSubmission.domain.User;
 import com.coderscampus.AssignmentSubmission.dto.AuthCredentialsRequest;
+import com.coderscampus.AssignmentSubmission.service.AssignmentService;
 import com.coderscampus.AssignmentSubmission.util.JwtUtil;
+
+import antlr.Token;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 //import com.coderscampus.AssignmentSubmission.filter.JwtUtil;
 
 //@Api(tags = "Authentication")
@@ -37,8 +57,8 @@ private  JwtUtil jwtUtili;
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }*/
-
-    @PostMapping("login")
+   //@CrossOrigin
+   @PostMapping("login")
     public ResponseEntity<?> login(@RequestBody  AuthCredentialsRequest request) {
         try {
             Authentication authenticate = authenticationManagerr
@@ -60,15 +80,38 @@ private  JwtUtil jwtUtili;
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
+   
+   // we will make function to get request for data "jwt" 
+   //data like expiring 
+   //7
+   //localhost:8080/api/auth/validaye?token=blahblahblah
+   //to test this 
+   /*we should put Bearer token*/
+   @GetMapping("/validate")
+   public ResponseEntity<?> validateToken1(@RequestParam String token,@ AuthenticationPrincipal User user) throws ExpiredJwtException 
+   {	//org.jboss.logging.Logger logger = LoggerFactory.logger(JwtUtil.class);
 
-    @PostMapping("view")
+	   try {
+	   Boolean isTokenValid= jwtUtili.vaildateToken(token, user);
+        return ResponseEntity.ok(isTokenValid);
+	   }
+	  
+   catch (ExpiredJwtException ex) {
+	   return ResponseEntity.ok(false);
+          } 
+
+      
+
+
+}
+    //@CrossOrigin
+    @GetMapping("view")
     public String hh() {
     	return "ffffffffffffffffffff";
     	
     }
-
-
-}
+	
+  }
 
 
 
