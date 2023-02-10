3cbd40914aa687b14ca00cca935560e044ef1544
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-02-10 05:19:24 +0200

    Update JwtUtil.java

--- src/main/java/com/coderscampus/AssignmentSubmission/util/JwtUtil.java
+++ src/main/java/com/coderscampus/AssignmentSubmission/util/JwtUtil.java
@@ -18,31 +18,34 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtUtil implements Serializable{
 private static final long serialVersionUID=-2550185165626007488L;
-public static final long JWT_TOKEN_VALIDITY=30*24 *60*60;
+public static final long JWT_TOKEN_VALIDITY = 120; //30*24 *60*60;
 @Value("${jwt.secret}")
 private String secret;	
 public String getUsernameFormToken(String token) {
 	
 	return getClaimFromToken(token,Claims::getSubject);
 	}
+//jwt expiration
 public Date getIssuedAtDateFormToken(String token) {
-	return  (Date) getClaimFromToken(token,Claims::getIssuedAt);
+	return   getClaimFromToken(token,Claims::getIssuedAt);
 	
-}
+}//jwt expiration
 public Date getExpirationDateFromToken(String token) {
-	return (Date) getClaimFromToken(token,Claims::getExpiration);
+	return  getClaimFromToken(token,Claims::getExpiration);
 	
 }
 public<T> T getClaimFromToken(String token,Function<Claims,T>claimsResolver) {
 	final Claims claims = getAllClaimsFromToken(token);
 	return claimsResolver.apply(claims);
 }
+//jwt expiration
 private Claims getAllClaimsFromToken(String token) {
 	return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
 }
+//jwt expiration
 private Boolean isTokenExpired(String token)
 {final Date expiration =getExpirationDateFromToken(token);
-return expiration.before(new Date(0));
+return expiration.before(new Date());
 }
 private Boolean ignoreTokenExpiration(String Token) {
 	//here yoy specify tokens,for that the expiration is ignored
@@ -61,9 +64,10 @@ private String doGenerateToken(Map<String,Object>claims,String subject) {
 public Boolean canTokenBeRefreshed(String token) {
 	return (!isTokenExpired(token)||ignoreTokenExpiration(token));
 }
+//jwt expiration
 public Boolean vaildateToken(String token,UserDetails userDetails) {
 	final String username=getUsernameFormToken(token);
-	return (username.equals(userDetails.getUsername())&&!isTokenExpired(token));
+	return (userDetails !=null && username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 }
 
 }
